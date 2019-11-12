3f353789ad3a3c31467525349c6b032149c1942c
phockett <phockett@users.noreply.github.com>
2019-11-12 12:20:39 -0500

    Shell script debug.

--- shell/jr_epsProc_nb.sh
+++ shell/jr_epsProc_nb.sh
@@ -8,7 +8,7 @@
 echo Starting Jupyter-runner batch run with nohup
 
 # Set env
-conda activate ePSproc
+conda activate ePSproc-v1.2
 cd $1
 
 nohup jupyter-runner --overwrite --workers $2 --parameter-file=$3 --format notebook --allow-errors $4 &
