8cc799d78c116c253041b59a68a6e745a9f2b077
phockett <phockett@users.noreply.github.com>
2021-03-29 15:28:28 -0400

    Added frame rotations and pyGamess wrapper.
    Basics are working, but still a bit messy and needs some work to 
    implement symmetry correctly.

--- epsman/elecStructure/gamess.py
+++ epsman/elecStructure/gamess.py
@@ -7,6 +7,7 @@ Libraries:
 
 - `pygamess <https://github.com/kzfm/pygamess>`_ (also `on pypi <https://pypi.org/project/pygamess/>`_)
 - `RDKit <https://rdkit.org/docs/index.html>`_
+- `PubChemPy <https://pypi.org/project/PubChemPy/>`_
 
 Aims here:
 
@@ -17,6 +18,7 @@ Aims here:
 """
 
 from pygamess import Gamess
+import numpy as np
 
 from pathlib import Path
 
@@ -35,6 +37,10 @@ try:
 except ImportError:
     print("*** PubChemPy not available, molecule download functions will not be available.")
 
+try:
+    import pandas as pd
+except ImportError:
+    print("*** Pandas not available, fancy tabulated data display will not be available.")
 
 
 class ESgamess():
@@ -60,7 +66,7 @@ class ESgamess():
     __notebook__ = isnotebook()
 
 
-    def __init__(self, searchName = None, smiles = None, molFile = None, addH = False, verbose = 1):
+    def __init__(self, searchName = None, smiles = None, molFile = None, addH = False, job = None, sym = None, verbose = 1):
 
         self.verbose = verbose
 
@@ -73,6 +79,12 @@ class ESgamess():
         self.molFromPubChem()
         self.molFromSmiles(addH = addH)
         self.molFromFile()
+
+        # Additional vars for Gamess job
+        # Set default to None to allow easy overwrite later... or set to 'Default' and 'C1' for always running config?
+        # Issue is as usual - when should these be set to defaults, and should they always be overwritten by new input by default?
+        self.setAttribute('job', job)
+        self.setAttribute('sym', sym)
 
         # Display some output
 
@@ -191,6 +203,31 @@ class ESgamess():
         # return mol
 
         # TODO: add disply checking functionality.
+        # Alternatively, just return mol object
+        if self.__notebook__:
+            display(self.mol)
+
+    def plot2D(self, atomIndex=True):
+        """2D plot using RDkit functionality, with optional atom index labels"""
+
+        if self.__notebook__:
+            if atomIndex:
+                display(self.mol_with_atom_index())
+            else:
+                display(self.mol)
+
+
+    def plot3D(self, style='stick'):
+        """
+        Thin wrapper for RDkit AllChem.Draw.IPythonConsole.drawMol3D(self.mol), needs py3Dmol library.
+
+        Style is one of 'stick' (default), 'sphere', 'line', 'cross', 'cartoon'.
+        See the `3Dmol docs for details <https://3dmol.csb.pitt.edu/doc/index.html>`__
+
+        """
+
+        if self.__notebook__:
+            AllChem.Draw.IPythonConsole.drawMol3D(self.mol,drawAs=style)
 
 
     def setBondLength(self, bonds = None):
@@ -199,11 +236,100 @@ class ESgamess():
 
         Pass dictionary with items giving atom1, atom2 and the bond length to set.
         E.g. bonds = {'CCBond':{'a1':0, 'a2':1, 'l':10}}
+
+        TODO: Add passing as a table or array for brevity, format [a1, a2, l; ....]
+
         """
 
         if bonds is not None:
-            for bond in bonds.items():
+            for k, bond in bonds.items():
                 AllChem.SetBondLength(self.mol.GetConformer(),bond['a1'],bond['a2'],bond['l'])
+
+        print("*** Set bonds, new coord table:")
+        self.printTable()
+
+
+    def rotateFrame(self, rotations = {'y':np.pi/2}, canonicalise=True):
+        """
+        Rotate molecule to reference frame, using rotation matricies + AllChem.TransformConformer() functionality.
+
+        CODE ADAPTED FROM `Github user iwatobipen's example notebook <https://nbviewer.jupyter.org/github/iwatobipen/playground/blob/master/rotation_mol.ipynb>`__
+        Thanks to `iwatobipen <https://github.com/iwatobipen>`__ and the `RDkit list <https://sourceforge.net/p/rdkit/mailman/message/36598250/>`__
+
+
+        Parameters
+        ----------
+        rotations : dict, default = {'y':np.pi/2}
+            Supply rotations as a dict, with rotations about labelled axes.
+            Rotations will be applied sequentially.
+            E.g. {'x':np.pi/2, 'y':np.pi/2} will rotate by pi/2 about x-axis then pi/2 about (new) y-axis.
+
+        canonicalise : bool, default = True
+            Canonicalise before transform?
+            This runs :py:func:`rdkit.Chem.rdMolTransforms` (see `RDkit docs <https://rdkit.org/docs/source/rdkit.Chem.rdMolTransforms.html#module-rdkit.Chem.rdMolTransforms>`__.)
+
+        All transforms are applied to self.mol.
+
+
+        Notes
+        -----
+        Default case sets then rotates "canonical" form by pi/2 about y
+
+        - Canoncial form sets x-axis as highest sym axis, see https://rdkit.org/docs/source/rdkit.Chem.rdMolTransforms.html#module-rdkit.Chem.rdMolTransforms
+        - Then rotate x > z axis, to match Gamess/comp chem conventions.
+
+        """
+
+        # Canonicalise? This sets x-axis as highest sym axis, see https://rdkit.org/docs/source/rdkit.Chem.rdMolTransforms.html#module-rdkit.Chem.rdMolTransforms
+        if canonicalise:
+            AllChem.CanonicalizeConformer(self.mol.GetConformer())
+
+        # Define rotations
+        def rot_ar_x(radi):
+            return  np.array([[1, 0, 0, 0],
+                              [0, np.cos(radi), -np.sin(radi), 0],
+                              [0, np.sin(radi), np.cos(radi), 0],
+                             [0, 0, 0, 1]], dtype=np.double)
+
+        def rot_ar_y(radi):
+            return  np.array([[np.cos(radi), 0, np.sin(radi), 0],
+                              [0, 1, 0, 0],
+                              [-np.sin(radi), 0, np.cos(radi), 0],
+                             [0, 0, 0, 1]], dtype=np.double)
+
+        def rot_ar_z(radi):
+            return  np.array([[np.cos(radi), -np.sin(radi), 0, 0],
+                              [np.sin(radi), np.cos(radi), 0, 0],
+                              [0, 0, 1, 0],
+                             [0, 0, 0, 1]], dtype=np.double)
+        # tforms = {0: rot_ar_x, 1: rot_ar_y, 2: rot_ar_z}
+        tforms = {'x': rot_ar_x, 'y': rot_ar_y, 'z': rot_ar_z}
+
+        # Loop over defined rotations
+        for k,v in rotations.items():
+            AllChem.TransformConformer(self.mol.GetConformer(), tforms[k](v))
+
+
+    def printTable(self):
+        """
+        Show pretty table using Pandas (use printCoords() for simple version).
+
+        Code is very similar to printCoords(), based on pyGamess routines (atom_section, https://github.com/kzfm/pygamess/blob/master/pygamess/gamess.py)
+
+        TODO: amalgamate these!
+        """
+
+        conf = self.mol.GetConformer(0)
+        atomList = []
+        for atom in self.mol.GetAtoms():
+            pos = conf.GetAtomPosition(atom.GetIdx())
+
+            atomList.append([atom.GetIdx(), atom.GetSymbol(), atom.GetAtomicNum(), pos.x, pos.y, pos.z])
+
+        self.pdTable = pd.DataFrame(atomList, columns=['Ind','Species','Atomic Num.','x','y','z'])
+
+        if self.__notebook__:
+            display(self.pdTable)
 
 
     def printCoords(self):
@@ -222,3 +348,107 @@ class ESgamess():
             section += "{:<3} {:>4}.0   {:> 15.10f} {:> 15.10f} {: 15.10f} \n".format(atom.GetSymbol(), atom.GetAtomicNum(), pos.x, pos.y, pos.z)
 
         print(section)
+
+    def initGamess(self, gamess_path = '/usr/local/gamess', **kwargs):
+        """
+        Init Gamess object & build (default) input card.
+
+        All kwargs are passed to pyGamess init.
+
+        This minimally requires `gamess_path`, which defaults to '/usr/local/gamess'
+
+        """
+
+        # Init pyGames object
+        self.g = gamessInput(job = self.job, sym = self.sym, gamess_path = gamess_path, **kwargs)
+
+        # Set pointer to options
+        self.params = self.g.options
+
+        # Show current Gamess input
+        print("*** Gamess input card:")
+        print(self.g.input(self.mol))
+
+        # self.gCard = {}
+        # self.gCard['default'] = self.g.input(self.mol)
+        #
+        # print("*** Gamess input card set to self.gCard['default']")
+        # print(self.gCard['default'])
+
+
+    def setGamess(self, job = None, note = None, sym = None):
+        """
+        Set up a new Gamess input card.
+
+        Parameters
+        ----------
+        job : str, default = None
+            Name for input card, defaults to self.name
+
+        note : str, default = None
+            Note to push to input card, defaults to `job, basis`
+
+        sym : str, default = 'C1'
+            Set symmetry for job.
+
+        """
+
+        if job is None:
+            # job = f'{self.name}, {self.g.options['basis']['gbasis']}'
+            job = self.name
+
+        if note is None:
+            note = f"{job}, {self.g.options['basis']['gbasis']}"
+
+        # Set all inputs - THIS MIGHT work, but relies on knowing pyGamess input structure - may be better to wrap key params instead?
+        # self.g.setAttributesFromDict(**kwargs, overwriteFlag = True)
+
+        # self.gCard[job] = self.g.input(self.mol, job = note, sym = sym)
+        #
+        # print(f"*** Gamess input card set to self.gCard['{job}']")
+        # print(self.gCard[job])
+
+        # Show current Gamess input
+        print("*** Gamess input card:")
+        print(self.g.input(self.mol, job = note, sym = sym))
+
+
+    # def runGamess(self, job = 'Default', sym = 'C1', **kwargs):
+    def runGamess(self, **kwargs):
+        """ Wrapper for pyGamess.run(), using self.mol and additional input options. """
+
+        # # Additional vars for Gamess job
+        # self.setAttribute('job', job)
+        # self.setAttribute('sym', sym)
+        # self.setGamess(**kwargs)
+
+        self.g.run(self.mol)
+
+
+
+
+
+
+# Try subclassing pyGamess routine for extra control over input...
+class gamessInput(Gamess):
+    from epsman._util import setAttribute, setAttributesFromDict
+
+    def __init__(self, job = None, sym = None, **kwargs):
+
+        super().__init__(**kwargs)
+        self.debug = True
+        self.verbose = None
+
+        # Additional vars for Gamess job - may want to push to self.options?
+        self.setAttribute('job', job)
+        self.setAttribute('sym', sym)
+
+    def input(self, mol, job = None, sym = None, overwriteFlag = True):
+        """Wrap input writer for additional parameters"""
+
+        self.setAttribute('job', job, overwriteFlag)
+        self.setAttribute('sym', sym, overwriteFlag)
+
+        return "{0} $DATA\n{1}\n{2}\n{3} $END\n".format(self.print_header(),
+                                                        self.job, self.sym,
+                                                        self.atom_section(mol))
