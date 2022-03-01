b76606fb0a00a608f8c6fffa38b2647629d48c71
phockett <phockett@users.noreply.github.com>
2022-03-01 14:53:27 -0500

    Updated Intel runner script.

--- epsman/shell/ePS_batch_nohup-intel.sh
+++ epsman/shell/ePS_batch_nohup-intel.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Wrapper for ePS batch jobs for remote run with Fabric
-# 23/10/19
+# 09/09/21  v2, with updated output file.
+# 23/10/19  v1
 
 echo Starting batch run with nohup
 
-# nohup /home/paul/ePS/jobs/ePS_batch_job.sh &
-# nohup /home/paul/ePS/jobs/ePS-3885d87_batch_job.sh &
-nohup source /home/paul/ePS/jobs/ePS_batch_job-intel.sh &
+jobConfFile=$1
+source $jobConfFile  # 23/08/20 Settings for local dirs now passed to main script
+
+# For intel case need to set env - NOTE THIS SHOULD BE PASSED (via jobConfFile)
+source /opt/ePolyScat.3885d87/ePS_v3885d87_env.sh
+
+# Currently set for jobPath, but should change to shell scrip repo?
+nohup $scpdir/ePS_batch_job.sh $1 > $jobConfFile.nohup.log 2>&1 &
